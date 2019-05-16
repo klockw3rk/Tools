d5482b2f9c830cc0a571b1026f2363e87a0a2713
klockw3rk <samuel.h.whang@gmail.com>
2019-05-16 17:03:44 -0400

    Powershell script to convert base64 into a file

---
+++ base64_decode_file.ps1
@@ -0,0 +1,11 @@
+﻿#Insert Base64 hash value surrounded by single quotes
+$b64 = ''
+
+#Insert desired file output name. Full path is desired
+$filename = 'C:\path\to\final\destination\test.txt'
+
+#This bit of code converts base64 string into readable bytes
+$bytes = [Convert]::FromBase64String($b64)
+
+#This bit of code writes the bytes into a file.
+[IO.File]::WriteAllBytes($filename, $bytes)
